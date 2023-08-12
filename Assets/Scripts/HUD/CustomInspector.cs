44a0b65013921268d310355d7650d4ca5b9ec050
Gustavo Araújo <gus.araujo173@gmail.com>
2023-08-11 21:19:20 -0300

    Added HUD functionality

---
+++ Assets/Scripts/HUD/CustomInspector.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(UpdateScore))]
+public class CustomInspector : Editor{
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+
+        UpdateScore panel = (UpdateScore)target;
+
+        if(GUILayout.Button("Click Here")){
+            panel.UpdateText(12567897);
+        }
+    }
+}
