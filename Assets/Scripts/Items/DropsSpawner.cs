149d0d0baab92687205ef9d6edea7eaa945f27a0
Carlos Bravo <carlosh.serrado@gmail.com>
2023-09-07 14:04:44 -0300

    Added probability to each item

--- Assets/Scripts/Items/DropsSpawner.cs
+++ Assets/Scripts/Items/DropsSpawner.cs
@@ -9,6 +9,7 @@ public struct DropsOptions
 {
     public bool canSpawn;
     public GameObject itemPrefab;
+    public int probability;
     public UnityEvent OnPickup;
 }
 
@@ -16,17 +17,21 @@ public class DropsSpawner : MonoBehaviour
 {
     [Header("Items")]
     [SerializeField] private DropsOptions[] itemsOptions;
+    private int[] itemsProbabilitySum;
     private ObjectPool<ItemController>[] itemPool;
-    [SerializeField] private float itemSpawnChance;
 
     private bool isGamePaused;
 
     private void Start()
     {
         itemPool = new ObjectPool<ItemController>[itemsOptions.Length];
+        itemsProbabilitySum = new int[itemsOptions.Length];
+        int sum = 0;
         for (int i = 0; i < itemsOptions.Length; i++)
         {
             itemPool[i] = createObjectPool(itemsOptions[i], i, 1, 5);
+            sum += itemsOptions[i].probability;
+            itemsProbabilitySum[i] = sum;
         }
     }
 
@@ -60,9 +65,17 @@ public class DropsSpawner : MonoBehaviour
 
     public void SpawnItem(Vector2 startingPosition)
     {
-        if (Random.Range(0f, 1f) > itemSpawnChance) return;
-
-        int itemIndex = Random.Range(0, itemsOptions.Length);
+        int random = Random.Range(0, 100);
+        int itemIndex = -1;
+        for (int i = 0; i < itemsProbabilitySum.Length; i++)
+        {
+            if (random < itemsProbabilitySum[i])
+            {
+                itemIndex = i;
+                break;
+            }
+        }
+        if (itemIndex == -1) return;
         ItemController item = itemPool[itemIndex].Get();
         item.OnReuseObject(startingPosition);
     }
@@ -78,7 +91,8 @@ public class DropsSpawner : MonoBehaviour
         itemPool[itemIndex].Release(itemController);
     }
 
-    public void PauseDrops(bool isGamePaused){
+    public void PauseDrops(bool isGamePaused)
+    {
         this.isGamePaused = isGamePaused;
     }
 }
