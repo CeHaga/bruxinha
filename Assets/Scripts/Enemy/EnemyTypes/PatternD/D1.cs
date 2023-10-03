66073074c8e00bbaaea9243990721f95243a5841
Carlos Bravo <carlosh.serrado@gmail.com>
2023-10-03 18:24:19 -0300

    Changed pattern A

--- Assets/Scripts/Enemy/EnemyTypes/PatternD/D1.cs
+++ Assets/Scripts/Enemy/EnemyTypes/PatternD/D1.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 public class D1 : EnemyController
 {
+	[SerializeField] private float height = 10f;
 	public override EnemyState Move(float t)
 	{
 		if (t < 160)
 		{
-			Vector2 position = new Vector2(120 - t / 4 * 20 / Mathf.PI, 10 * Mathf.Cos(t / 4));
+			Vector2 position = new Vector2(120 - t / 4 * 20 / Mathf.PI, height * Mathf.Cos(t / 4));
 			return new EnemyState(position, flying, true);
 		}
 		return new EnemyState(null, dying, false);
--- Assets/Scripts/Enemy/EnemyTypes/PatternD/D3.cs
+++ Assets/Scripts/Enemy/EnemyTypes/PatternD/D3.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 public class D3 : EnemyController
 {
+	[SerializeField] private float height = 10f;
 	public override EnemyState Move(float t)
 	{
 		if (t < 160)
 		{
-			Vector2 position = new Vector2(120 - t / 4 * 20 / Mathf.PI, -40 + 10 * Mathf.Cos(t / 4));
+			Vector2 position = new Vector2(120 - t / 4 * 20 / Mathf.PI, -40 + height * Mathf.Cos(t / 4));
 			return new EnemyState(position, flying, true);
 		}
 		return new EnemyState(null, dying, false);
